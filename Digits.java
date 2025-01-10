1863123860f1cc1e2ece09ef88eb9d9ec19bda72
Naresh-T-1308 <naresht1304@gmail.com>
2025-01-10 15:17:23 +0530

    Update Digits.java

--- Digits.java
+++ Digits.java
@@ -1,7 +1,7 @@
 public class Digits {
 
 	public static void main(String[] args) {
-System.out.println("  count program");
+System.out.println(" digit count program");
 		int i=123;
 		int j;
 		int count=0;
